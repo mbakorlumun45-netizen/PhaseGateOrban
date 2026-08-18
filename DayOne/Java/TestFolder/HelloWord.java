911c5f3ed07c5afd7962f22b0bf0aad111ede60a
mbakorlumun4-netizen <mbakorlumun45@gmail.com>
2026-08-18 08:03:53 +0100

    day one folder comited

---
+++ DayOne/Java/TestFolder/HelloWord.java
@@ -0,0 +1,12 @@
+public class HelloWord{
+    public static void main(String[] args){
+
+    
+    String name = "HELLO WORD";
+    for(int count = 1; count <= 5; count++){
+    
+    System.out.println(name);
+    }
+
+}
+}
---
+++ DayOne/Java/TestFolder/JavaCmmands.java
@@ -0,0 +1,21 @@
+java -cp "junit-platform-console-standalone-1.11.0.jar;out" org.junit.platform.console.Console
+Launcher --scan-class-path
+
+
+java -cp "junit-platform-console-standalone-1.11.0.jar:out" org.junit.platform.console.Console
+Launcher --scan-class-path
+
+
+
+
+cd ~/javaTasks/tdd
+
+
+
+
+
+1. Compile with the JAR on the classpath
+javac -cp .:junit-platform-console-standalone-1.13.4.jar TestClass.java
+
+# 2. Run using the 'execute' subcommand and explicit class selection
+java -jar junit-platform-console-standalone-1.13.4.jar execute --class-path . --select-class TestClass --details=verbose
